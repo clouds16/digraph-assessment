559e90672ea2bbd50db5edca25ae6b4de2d311f4
Hector Alvarez-Toledo <hect16@gmail.com>
2022-02-11 15:33:21 -0600

    Added global state using create context

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,18 @@ import './App.css';
 //import Graph from './graph/Graph'
 import GetGraphs from './components/GetGraphs'
 import Graph from './creategraph/Graph'
+import AppContext from './store/AppContext'
 
 function App() {
+  
+  const myContext =  useContext(AppContext);
+
+
   return (
     <div className="App">
     <h1> Graph Application </h1>
     <div className="w-screen h-screen flex flex-row justify-center place-items-center bg-red-100"> 
-      <Graph />
+      <Graph graphData = {myContext.CURRENTWORKFLOW} />
       
       <div className="flex flex-col place-items-start justify-start">
         <button className="m-2 p-2 border-black bg-white"> Add new Workflow </button>
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppContext from './store/AppContext'
+
+let [backEnd , setBackEnd] = useState('http://localhost:3100/')
+let [currentWorkflow , setCurrentWorkflow] = useState({
+  name : "",
+  nodes : [],
+  edges : []
+})
+
+
+let Store = {
+  BACKEND : backEnd,
+  SETBACKEND : setBackEnd,
+  CURRENTWORKFLOW : currentWorkflow ,
+  setCurrentWorkflow : setCurrentWorkflow
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AppContext.Provider value={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AppContext.Provider>
+,
   document.getElementById('root')
 );
 
